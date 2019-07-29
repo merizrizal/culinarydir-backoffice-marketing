877c372e3027dc97d0eb9104390ba631d60bd302
AxelRj13 <axel.raharja1@gmail.com>
2019-07-29 16:37:09 +0700

    penyesuaian user akses app module

--- controllers/BusinessController.php
+++ controllers/BusinessController.php
@@ -1076,7 +1076,6 @@ class BusinessController extends \backoffice\controllers\BaseController
 
                                     $modelUserAkses = UserAkses::find()
                                         ->andWhere(['user_level_id' => $modelUserRole->user_level_id])
-                                        ->andWhere(['is_active' => true])
                                         ->asArray()->all();
 
                                     foreach ($modelUserAkses as $dataUserAkses) {
@@ -1085,7 +1084,7 @@ class BusinessController extends \backoffice\controllers\BaseController
                                         $modelUserAksesAppModule->unique_id = $modelUsers[$i]->id . '-' . $dataUserAkses['user_app_module_id'];
                                         $modelUserAksesAppModule->user_id = $modelUsers[$i]->id;
                                         $modelUserAksesAppModule->user_app_module_id = $dataUserAkses['user_app_module_id'];
-                                        $modelUserAksesAppModule->is_active = true;
+                                        $modelUserAksesAppModule->is_active = $dataUserAkses['is_active'];
                                         $modelUserAksesAppModule->used_by_user_role = [$modelUserRole->unique_id];
 
                                         if (!($flag = $modelUserAksesAppModule->save())) {
